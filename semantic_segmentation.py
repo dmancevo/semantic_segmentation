43dc6e1c1f51515fa4dad99cf314c327346be316
Diego A. Mancevo <dmancevo@gmail.com>
2016-05-15 15:45:10 +0200

    here is an example of deconvolution for upsampling

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -3,11 +3,24 @@ import scipy.misc
 import scipy.io
 import tensorflow as tf
 
+def center(image, mean_pixel):
+  return image - mean_pixel
+
+def un_center(image, mean_pixel):
+  return image + mean_pixel
+
+def _weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def _bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
 def _conv_layer(input, weights, bias):
   conv = tf.nn.conv2d(input, tf.constant(weights),
     strides=(1, 1, 1, 1), padding='SAME')
   return tf.nn.bias_add(conv, bias)
-
 
 def _pool_layer(input):
   return tf.nn.max_pool(input, ksize=(1, 2, 2, 1),
@@ -36,7 +49,7 @@ layers = (
       'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
 
       'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
-      'relu5_3', 'conv5_4', 'relu5_4'
+      'relu5_3', 'conv5_4', 'relu5_4', 'pool5',
   )
   
 
@@ -44,6 +57,11 @@ layers = (
 VGG_PATH = 'imagenet-vgg-verydeep-19.mat'
 data = scipy.io.loadmat(VGG_PATH)
 weights = data['layers'][0]
+
+#Load mean pixel values so we can center pictures
+#(This is done on the VGG paper as well)
+mean = data['normalization'][0][0][0]
+mean_pixel = np.mean(mean, axis=(0, 1))
 
 #Create placeholder for image (I'm using an arbitrary image here)
 im = scipy.misc.imread("bici.jpg").astype(np.float)
@@ -77,20 +95,27 @@ for i, name in enumerate(layers):
     current = _pool_layer(current)
   net[name] = current
 
-#Add one more dimention to match tensor input dimensions
+#Center image and add batch dimension
+im = center(im,mean_pixel)
 im = im[np.newaxis,:,:,:]
 
-#Store image representation on layer
-print net['relu5_4'].eval(feed_dict={input_image: im},session=sess).shape
-  
+#Image representation on layer relu1_1
+print net['relu1_1'].eval(feed_dict={input_image: im},session=sess).shape
+
   
 ##########################################
 #########ADD DECONVOLUTION LAYERS#########
 ##########################################
 
-deConv_net = {}
+#input = (1, 523, 769, 64) from layer relu1_1 (later we need to check the input from images on our data set)
+#output = (input + stride - 1)
+deconv = tf.nn.conv2d_transpose(value=net['relu1_1'], filter=_weight_variable((16, 16, 3, 64)),output_shape=(1, 523, 769, 3),
+strides=(1, 1, 1, 1), padding='SAME')
 
+sess.run(tf.initialize_all_variables())
 
+out = deconv.eval(feed_dict={input_image: im},session=sess)[0]
+scipy.misc.imsave('deconv_out.png',out)
 
 
 
